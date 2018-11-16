ca79713f0603df69d2e050185cf73cb4ba0967b7
jodyneckles <jody@technative.tv>
2018-11-16 18:11:33 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
-def alphabetize(arr)
-  # code here
+require "pry"
+
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(ary)
+	hash = {}
+  ary.each do |text|
+  	new_text = ""
+  	text.each_char { |chr| new_text << ALPHABET.index(chr) if ALPHABET.include?(chr)}
+  	hash[text] = new_text
+  end
+  ary.sort_by { |text| hash[text] }
 end
