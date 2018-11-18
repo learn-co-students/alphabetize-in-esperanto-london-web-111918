a1747fd90f09912123c0661265c3236fd20678ca
Claudia Fox <claudiafox95@icloud.com>
2018-11-18 20:25:33 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esperanto_arr = esperanto_alphabet.chars
+  arr.sort_by do |words|
+    words.split('').collect do |letter|
+      esperanto_arr.index(letter)
+    end
+  end
 end
